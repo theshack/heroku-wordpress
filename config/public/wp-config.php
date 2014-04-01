a1051d2e82c2f0164bf8961e99ab60b89f07521d
Anthony Garand <anthonygarand@gmail.com>
2014-04-01 12:54:30 -0400

    Remove old DB config

--- config/public/wp-config.php
+++ config/public/wp-config.php
@@ -20,19 +20,15 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define("DB_NAME", trim($url["path"], "/"));
-// define("DB_NAME", "heroku_4eed1f5663e035f");
 
 /** MySQL database username */
 define("DB_USER", trim($url["user"]));
-// define("DB_USER", "b7cfc877a785ae");
 
 /** MySQL database password */
 define("DB_PASSWORD", trim($url["pass"]));
-// define("DB_PASSWORD", "db3717c2");
 
 /** MySQL hostname */
 define("DB_HOST", trim($url["host"]));
-// define("DB_HOST", "us-cdbr-east-03.cleardb.com");
 
 /** MySQL database port  */
 // define("DB_PORT", trim($url["port"]));
